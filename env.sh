9030fb57183469b0824df347d8c885db10ecd633
willianreis89 <46463250+willianreis89@users.noreply.github.com>
2026-01-02 15:47:13 -0300

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,10 +1,1 @@
-VIRTUAL_ENV=/opt/friday/venv
-PATH=/opt/friday/venv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-LD_LIBRARY_PATH=/opt/friday/venv/lib/python3.11/site-packages/nvidia/cudnn/lib:/opt/friday/venv/lib/python3.11/site-packages/nvidia/cublas/lib:/usr/lib/x86_64-linux-gnu
-
-CUDA_VISIBLE_DEVICES=0
-CUDA_DEVICE_ORDER=PCI_BUS_ID
-
-HA_URL=http://172.30.156.7:8123
-HA_TOKEN=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI4ZjlhMGNiZTAyOTc0YTM3ODVmMmE4ZWRjNzFlOWI3NyIsImlhdCI6MTc2NzAzODc1OSwiZXhwIjoyMDgyMzk4NzU5fQ.RZhAM1woIHToslJJ5Kzzhj-VGqBTs4Y4lFGIRvKHZhM
